a0cb3cb179e4851f05fca7f75ed5ebc35136e0fc
Anish Choudhury <anish.choudhury@student.allenisd.org>
2025-03-20 12:50:47 -0500

    SLICKpricingUPDATE

--- src/components/PricingBox.jsx
+++ src/components/PricingBox.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import '../styles/PricingBox.css';
 import { FaArrowRight } from "react-icons/fa6";
 import { useNavigate } from 'react-router-dom';
+import whiteCheckmark from '../assets/whiteCheckmark.png'
+
 
 function PricingBox() {
 
@@ -14,13 +16,36 @@ function PricingBox() {
                 <div className='card'>
                     <div className='card-content'>
                         <h3>Small School (&lt;500)</h3>
-                        <p><strong>Tier 1 (Map Only):</strong> $2,000/year</p>
-                        <p><strong>Tier 2 (Tier 1 + Website + Availability):</strong> $4,000/year</p>
-                        <p><strong>Tier 3 (Tier 2 + Events):</strong> $6,000/year</p>
+                        <h4><strong>Tier 1:</strong> $2,000/year</h4>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Map</p>
+                        </div>
+                        <h4><strong>Tier 2:</strong> $4,000/year</h4>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>All Tier 1 Perks</p>
+                        </div>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Website</p>
+                        </div>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Availability</p>
+                        </div>
+                        <h4><strong>Tier 3:</strong> $6,000/year</h4>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>All Tier 2 Perks</p>
+                        </div>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Events</p>
+                        </div>
                         <div className='buttonsPrices'>
                             <button className='frontButtonPrices' onClick={() => navigate('/demo')}>
                                 <p> Learn More </p>
-                                <FaArrowRight />
                             </button>
                         </div>
                     </div>
@@ -32,13 +57,36 @@ function PricingBox() {
             <div className='card'>
                     <div className='card-content'>
                         <h3>Medium School (500 - 1000)</h3>
-                        <p><strong>Tier 1 (Map Only):</strong> $3,000/year</p>
-                        <p><strong>Tier 2 (Tier 1 + Website + Availability):</strong> $6,000/year</p>
-                        <p><strong>Tier 3 (Tier 2 + Events):</strong> $9,000/year</p>
+                        <h4><strong>Tier 1:</strong> $3,000/year</h4>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Map</p>
+                        </div>
+                        <h4><strong>Tier 2:</strong> $6,000/year</h4>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>All Tier 1 Perks</p>
+                        </div>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Website</p>
+                        </div>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Availability</p>
+                        </div>
+                        <h4><strong>Tier 3:</strong> $9,000/year</h4>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>All Tier 2 Perks</p>
+                        </div>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Events</p>
+                        </div>
                         <div className='buttonsPrices'>
                             <button className='frontButtonPrices' onClick={() => navigate('/demo')}>
                                 <p> Learn More </p>
-                                <FaArrowRight />
                             </button>
                         </div>
                     </div>
@@ -48,13 +96,36 @@ function PricingBox() {
             <div className='card'>
                     <div className='card-content'>
                     <h3>Large School (1000 - 2500)</h3>
-                    <p><strong>Tier 1 (Map Only):</strong> $5,000/year</p>
-                    <p><strong>Tier 2 (Tier 1 + Website + Availability):</strong> $9,000/year</p>
-                    <p><strong>Tier 3 (Tier 2 + Events):</strong> $12,000/year</p>
+                    <h4><strong>Tier 1:</strong> $5,000/year</h4>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Map</p>
+                        </div>
+                        <h4><strong>Tier 2:</strong> $9,000/year</h4>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>All Tier 1 Perks</p>
+                        </div>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Website</p>
+                        </div>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Availability</p>
+                        </div>
+                        <h4><strong>Tier 3:</strong> $12,000/year</h4>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>All Tier 2 Perks</p>
+                        </div>
+                        <div className='perkContainer'>
+                            <img src={whiteCheckmark} alt="Checkmark" className='logoPrice' onClick={() => {navigate('/')}} />
+                            <p>Events</p>
+                        </div>
                         <div className='buttonsPrices'>
                             <button className='frontButtonPrices' onClick={() => navigate('/demo')}>
                                 <p> Learn More </p>
-                                <FaArrowRight />
                             </button>
                         </div>
                     </div>
