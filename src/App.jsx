b9e5cadcd2c3ee7d3b4ad2068e1d9620abbf17bc
Anish Choudhury <anish.choudhury@student.allenisd.org>
2025-04-27 19:47:14 -0500

    fix3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './screens/Home';
 import Demo from './screens/Demo';
 import Pricing from './screens/Pricing';
@@ -8,14 +8,12 @@ import ContactUs from './screens/ContactUs';
 function App() {
   return (
     <BrowserRouter basename="/EduPathLLC/">
-    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/demo" element={<Demo />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/contactus" element={<ContactUs />} />
       </Routes>
-    </Router>
     </BrowserRouter>
   );
 }
