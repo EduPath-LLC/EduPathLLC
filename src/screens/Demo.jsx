ab1d9ae4fdf7a86990ec9052db773ff0392e54b9
Anish Choudhury <anish.choudhury@student.allenisd.org>
2025-05-30 18:07:37 -0500

    added images and now mobile friendly

--- src/screens/Demo.jsx
+++ src/screens/Demo.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import '../styles/Demo.css';
+import LocationLock from '../assets/LocationLock.jpg';
+import ScheduleViewer from '../assets/ScheduleViewer.jpg';
+import SchoolEmail from '../assets/SchoolEmail.jpg';
+import Lock from '../assets/lock.png';
 
 function Demo() {
   return (
@@ -17,28 +21,36 @@ function Demo() {
 
         {/* Email Verification */}
         <section className="demoCard">
-          <div className="demoImagePlaceholder">[ App Screenshot Placeholder ]</div>
+          <div className="demoImagePlaceholder">
+          <img src={Lock} style={{height: '100%' }} />
+          </div>
           <h2>Email Verification</h2>
           <p>Only verified school emails are allowed. This ensures secure, student-only access to all features.</p>
         </section>
 
         {/* Location Lock */}
         <section className="demoCard">
-          <div className="demoImagePlaceholder">[ Map Lock Screenshot Placeholder ]</div>
+          <div className="demoImagePlaceholder">
+            <img src={LocationLock}/>
+          </div>
           <h2>Location Lock</h2>
           <p>Map features only unlock when you're physically on campus. No spoofing, no remote access.</p>
         </section>
 
         {/* School Email Only */}
         <section className="demoCard">
-          <div className="demoImagePlaceholder">[ School Email Login Placeholder ]</div>
+          <div className="demoImagePlaceholder">
+          <img src={SchoolEmail} />
+          </div>
           <h2>School Email Only</h2>
           <p>Access is restricted to school domains like <code>@yourschool.edu</code>. Outsiders are locked out by default.</p>
         </section>
 
         {/* Schedule Viewer */}
         <section className="demoCard">
-          <div className="demoImagePlaceholder">[ Schedule UI Placeholder ]</div>
+          <div className="demoImagePlaceholder">
+          <img src={ScheduleViewer} style={{height: '100%' }}/>
+          </div>
           <h2>Schedule Viewer</h2>
           <p>Quickly view your daily schedule and find your next class — all in one place.</p>
 
