c8b3a599623160d369c82d32ba28842d46943a1b
jkvelagapudi <jkvelagapudi@gmail.com>
2025-06-04 15:51:07 -0500

    reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './screens/Home';
 import Demo from './screens/Demo';
 import Pricing from './screens/Pricing';
 import ContactUs from './screens/ContactUs';
 
+function RedirectHandler({ children }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const redirect = sessionStorage.redirect;
+    if (redirect) {
+      sessionStorage.removeItem('redirect');
+      navigate(redirect, { replace: true });
+    }
+  }, [navigate]);
+
+  return children;
+}
+
 function App() {
   return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/demo" element={<Demo />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/contactus" element={<ContactUs />} />
-      </Routes>
+    <BrowserRouter>
+      <RedirectHandler>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/contactus" element={<ContactUs />} />
+        </Routes>
+      </RedirectHandler>
     </BrowserRouter>
   );
 }
